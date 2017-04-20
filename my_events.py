0a89cd5dd7f38980584eb8319ac58f7df987ff38
maylandertamas <maylandertamas@gmail.com>
2017-04-20 16:11:01 +0200

    changed menu and create event

--- my_events.py
+++ my_events.py
@@ -4,14 +4,13 @@ import functions
 import ui
 import main
 
-TITLE_LIST_FOR_CREATE = ["ID", "Name", "Description","Place","Starting", "Ending", "Type", "Creator"]
+TITLE_LIST_FOR_CREATE = ["ID", "Name", "Description","Place","Starting", "Ending", "Type"]
 
 def start():
     while True:
-        event_table = file_functions.read_from_file("Eventinfo.csv", "event")
         user_table = file_functions.read_from_file("Userinfo.csv", "user")
-        ui.print_result(functions.show_events_by_user("test@test.com", "my", event_table))
-        options = ["Search",
+        options = ["Show events",
+                   "Search",
                    "Create",
                    "Delete",
                    "Edit"]
@@ -21,15 +20,18 @@ def start():
         inputs = ui.get_inputs("Please enter a number:", "")
         option = inputs[0]
         if option == "1":
-            #functions.search()
+            show_events(main.email)
             pass
         elif option == "2":
-            #event_table = create_event
+            #functions.search()
             pass
         elif option == "3":
-            #delete_event()
+            create_event(file_functions.read_from_file("Eventinfo.csv", "event"), email)
             pass
         elif option == "4":
+            #delete_event()
+            pass
+        elif option == "5":
             #table = edit_event(event_table, ui.get_inputs("Please give me an ID to update table element: ",
                                                 #"Update line in table"))
             pass  
@@ -39,15 +41,23 @@ def start():
             raise KeyError("There is no such option.")
 
 
-def create_event(): # Andi
-    Id = functions.generate_random_eventID(file_functions.read_from_file)
+def show_events(email):
+    event_table = file_functions.read_from_file("Eventinfo.csv", "event")
+    ui.print_result(functions.show_events_by_user(email, "my", event_table))
+
+def create_event(file_table,user_email): # Andi
+    table = file_table
+    Id = functions.generate_random_eventID(table)
     new_list = []
     new_list.append(Id)
     while len(new_list) < len(TITLE_LIST_FOR_CREATE)-1:
-        for title in TITLE_LIST_FOR_CREATE:
-            new_list.append(ui.get_inputs("Give me {0}:".format(title)))
-    new_list.insert(7, "")
-    #file_functions.write_to_file("Eventinfo.csv", new_list)
+        for title in TITLE_LIST_FOR_CREATE[1:]:
+            new_list.append(ui.get_inputs("Give me {0}:".format(title), "Append database"))
+    new_list.append(str(1231))
+    new_list.append(str(user_email))
+    table.append(new_list)
+    print(new_list)
+    file_functions.write_to_file("Eventinfo.csv", table)
 
 def delete_event(): # Gabi
     pass
